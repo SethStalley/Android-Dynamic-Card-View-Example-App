17977770442921a5a05b59ce472e50fb3a9dba29
Seth <sethstalley@gmail.com>
2015-09-21 23:04:17 -0600

    Added twitter login

---
+++ AndroidParadiseGardens/app/src/main/java/com/paradisegardens/requi/paradisegardens/TwitterLogin.java
@@ -0,0 +1,66 @@
+package com.paradisegardens.requi.paradisegardens;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.internal.app.WindowDecorActionBar;
+import android.view.Window;
+
+import com.google.android.gms.appdatasearch.Feature;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class TwitterLogin extends Activity {
+
+    TwitterLoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig =  new TwitterAuthConfig("cAfQnAPnaUucICxsqMR7ATHdu", "FGStz2x9Hfa75YnRH4pIz96SBRTlBMUYurysmGFhgRC9MZFhgm");
+        Fabric.with(this, new Twitter(authConfig));
+
+        setContentView(R.layout.activity_twitter_login);
+
+
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+                Intent myIntent=new Intent(TwitterLogin.this,Main.class);
+                startActivity(myIntent);
+                finish();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
+
