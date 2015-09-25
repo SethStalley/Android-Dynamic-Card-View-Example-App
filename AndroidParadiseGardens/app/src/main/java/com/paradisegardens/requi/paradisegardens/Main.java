79b515447915271ace20c1d9efc0b77a4fa822ad
Seth <sethstalley@gmail.com>
2015-09-25 10:12:59 -0600

    Initial tab sample working

--- AndroidParadiseGardens/app/src/main/java/com/paradisegardens/requi/paradisegardens/Main.java
+++ AndroidParadiseGardens/app/src/main/java/com/paradisegardens/requi/paradisegardens/Main.java
@@ -3,31 +3,24 @@ package com.paradisegardens.requi.paradisegardens;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import io.karim.MaterialTabs;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
 public class Main extends AppCompatActivity {
-    //butterknife library to easily bind layouts
-    @InjectView(R.id.toolbar)
-    Toolbar mToolbar;
 
-    @InjectView(R.id.material_tabs)
-    MaterialTabs mMaterialTabs;
-
-    @InjectView(R.id.view_pager)
-    ViewPager mViewPager;
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Home","Events"};
+    int Numboftabs =2;
 
 
     /**
@@ -56,47 +49,57 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
-
-        //set title and action bar
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle(getString(R.string.title_activity_main));
 
 
 
-        //startTabsActivityIntent = new Intent(this, TabsActivity.class);
+        // Creating The Toolbar and setting it as the Toolbar for the activity
 
-        //tabs
-        // Initialize the ViewPager and set an adapter
-
-
-
-        System.out.println("Connecting to C# Server");
-
-//        client = new ClientController();
-//        client.sendMsg();
-
-        //load up the cards
-        mItems = new ArrayList<>(30);
-        for (int i = 0; i < 30; i++) {
-            mItems.add(String.format("Card number %02d", i));
-        }
-        mAdapter = new CardViewAdapter(mItems);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
 
 
-        //Menu Drawer stuff
-//        mNavigationDrawerFragment = (NavigationDrawerFragment)
-//                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-//
-//        // Set up the drawer.
-//        mNavigationDrawerFragment.setUp(
-//                R.id.navigation_drawer,
-//                (DrawerLayout) findViewById(R.id.drawer_layout));
-//
-        //fill the recyclerView with the cards
-//        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(mAdapter);
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ AndroidParadiseGardens/app/src/main/java/com/paradisegardens/requi/paradisegardens/Tab1.java
@@ -0,0 +1,17 @@
+package com.paradisegardens.requi.paradisegardens;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.tab1,container,false);
+        return v;
+    }
+}
